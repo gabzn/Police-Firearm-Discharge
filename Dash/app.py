f93792572c7d51edab1be88b15ade4558a847937
Gabriel Yan Zhen Wu <gabrielzhenwu@gmail.com>
2021-12-09 16:47:46 -0500

    Added more graphs

--- Dash/app.py
+++ Dash/app.py
@@ -1,7 +1,7 @@
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 from age_range import age_plot
-from percentage import percentage_plot
+from state import state_percentage_plot, state_total_plot
 from race_pies import race_local_plot, race_global_plot
 from weapon import weapon_plot
 from map_plot import scatter_map_plot
@@ -25,23 +25,33 @@ age_plot.update_layout(bargap=0.2,
                               'xanchor': 'center', 'yanchor': 'top'},
                        xaxis={'tickmode':'array', 'tickvals':np.arange(start=0, stop=120, step=5), 'ticktext':np.arange(start=0, stop=100, step=5)})
 
-percentage_plot.update_layout(bargap=0.1, title={'text': 'Shootings per 10,000 Population in Each State', 
+weapon_plot.update_layout(bargap=0.2, title={'text': 'Most Common Weapons Found', 'y': 0.9, 'x': 0.5, 
+                                          'xanchor': 'center', 'yanchor': 'top'},
+                                          xaxis={'categoryorder':'total descending'})
+
+state_percentage_plot.update_layout(bargap=0.2, title={'text': 'Shootings in every 10,000 Population in Each State', 
                                           'y': 0.9, 'x': 0.5, 
                                           'xanchor': 'center', 
-                                          'yanchor': 'top'})                        
+                                          'yanchor': 'top'},
+                                          xaxis={'categoryorder':'total descending'})           
 
-race_local_plot.update_layout(bargap=0.1, title={'text': 'Race Percentages Based on 9000+ Victims', 
+state_total_plot.update_layout(bargap=0.2, title={'text': 'Total Shootings in Each State', 
                                           'y': 0.9, 'x': 0.5, 
                                           'xanchor': 'center', 
-                                          'yanchor': 'top'})
+                                          'yanchor': 'top'},
+                                          xaxis={'categoryorder':'total descending'})             
 
-race_global_plot.update_layout(bargap=0.1, title={'text': 'Race Percentages Based on Population of Each Race', 
+race_local_plot.update_layout(title={'text': 'Race Percentages Based on 9000+ Victims', 
+                                          'y': 0.9, 'x': 0.5, 
+                                          'xanchor': 'center', 
+                                          'yanchor': 'top'},)
+
+race_global_plot.update_layout(title={'text': 'Race Percentages Based on Population of Each Race', 
                                           'y': 0.9, 'x': 0.5, 
                                           'xanchor': 'center', 
                                           'yanchor': 'top'}) 
 
-weapon_plot.update_layout(bargap=0.1, title={'text': 'Most Common Weapons Found', 'y': 0.9, 'x': 0.5, 
-                                          'xanchor': 'center', 'yanchor': 'top'})
+
 
 
 # Layout starts here
@@ -57,27 +67,28 @@ app.layout = dbc.Container([
         ], width={'size':'6'}),
 
         dbc.Col([
-            dcc.Graph(id='race_local_plot',figure=race_local_plot),
+            # dcc.Graph(id='race_local_plot',figure=race_local_plot),
+            dcc.Graph(id='weapon_plot', figure=weapon_plot)
         ], width={'size':'6'})
-    ], className='age_and_race_row pb-3'),
+    ], className='age_and_weapon_row pb-3'),
   
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='percentage_plot', figure=percentage_plot)
+            dcc.Graph(id='state_percentage_plot', figure=state_percentage_plot)
         ])
-    ], className='percentage_plot_row pb-3'),
-
+    ], className='state_percentage_plot_row pb-3'),
+    
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='weapon_plot', figure=weapon_plot)
+            dcc.Graph(id='state_total_plot', figure=state_total_plot)
         ])
-    ], className='weapon_plot_row pb-3'),
-
+    ], className='state_total_plot_row pb-3'),
+    
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='scatter_map_plot', figure=scatter_map_plot)
+            dcc.Graph(id='race_local_plot',figure=race_local_plot),
         ])
-    ], className='scatter_map_plot_row pb-3'),
+    ], className='race_local_plot_row pb-3'),
 
     dbc.Row([
         dbc.Col([
@@ -85,7 +96,37 @@ app.layout = dbc.Container([
         ])
     ], className='race_global_plot_row pb-3'),
 
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='scatter_map_plot', figure=scatter_map_plot)
+        ])
+    ], className='scatter_map_plot_row pb-3'),
+
+
 ], className='container-fliud')
+
+
+@app.callback(
+    [Output(component_id='', component_property=''),
+     Output(component_id='', component_property='')],
+    [Input(component_id='', component_property=''),
+     Input(component_id='', component_property='')],
+    prevent_initial_call=True
+)
+def update_my_graph(value):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
